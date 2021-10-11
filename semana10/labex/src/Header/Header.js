aad4155d704ca3dfc2255718a43582407c042698
Jessica <bentojessica48@gmail.com>
2021-10-11 19:24:48 -0300

    Projeto Labex - Pastas feitas e WireFrame

---
+++ semana10/labex/src/Header/Header.js
@@ -0,0 +1,10 @@
+ import {ContainerHeader} from './styled'
+ 
+ const Header = () => {
+    return (
+        <ContainerHeader>LabeX
+            <button>Home</button>
+        </ContainerHeader>
+    )
+}
+export default Header
---
+++ semana10/labex/src/Header/styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+export const ContainerHeader = styled.div`
+    border: 1px solid black;
+    display: flex;
+    justify-content: space-between;
+    padding: 20px;
+    margin: 0px;
+`
