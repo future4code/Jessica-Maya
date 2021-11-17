32d26ec0e5306fa5cad71b9959dfb705b262a92c
Jéssica <84422472+Bentojessica@users.noreply.github.com>
2021-11-17 17:07:58 -0300

    Atividades de Introdução Typescript

---
+++ semana15/Typescript/src/desafio1.ts
@@ -0,0 +1,10 @@
+// Exercicio 5
+
+
+function calculos(numero1: number, numero2: number): void {
+
+    console.log( numero1 + numero2)
+    console.log( numero1 - numero2)
+    console.log( numero1 * numero2)
+}
+calculos(2, 4)
---
+++ semana15/Typescript/src/exercicio-4.ts
@@ -0,0 +1,26 @@
+type pokemon = {
+	name: string,
+  types: string,
+	healthPoints: number
+}
+
+const pokemon1: pokemon = {
+  name: "Charmander",
+  types: "Fire",
+  healthPoints: 28
+}
+
+const pokemon2: pokemon = {
+  name: "Bulbasaur",
+  types: "Grass/Poison",
+  healthPoints: 31
+}
+
+const pokemon3: pokemon = {
+  name: "Squirtle",
+  types: "Water",
+  healthPoints: 35
+}
+
+// a) Como você faria, já com a extensão instalada, para gerar um arquivo javascript 
+//a partir do  arquivo typescript com o código abaixo?
