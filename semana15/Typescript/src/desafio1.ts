b5b224bdde1f54407e451b035869796dff979073
Jéssica <84422472+Bentojessica@users.noreply.github.com>
2021-11-18 17:47:34 -0300

    Adições de atividades

--- semana15/Typescript/src/desafio1.ts
+++ semana15/Typescript/src/desafio1.ts
@@ -3,8 +3,24 @@
 
 function calculos(numero1: number, numero2: number): void {
 
+    if (isNaN(numero1)) { 
+        return console.log("Invalid first number in argv")
+      }
+      
+      if (isNaN(numero2)) { 
+        return console.log("Invalid second number in argv")
+      }
+
     console.log( numero1 + numero2)
     console.log( numero1 - numero2)
     console.log( numero1 * numero2)
+
+    if (numero1 > numero2) {
+        console.log("Maior: ", numero1)
+      } else if (numero1 < numero2) {
+        console.log("Maior: ", numero2)
+      } else {
+        console.log(`São iguais: ${numero1} = ${numero2}`)
+      }
 }
 calculos(2, 4)
--- semana15/Typescript/src/exercicio-4.ts
+++ semana15/Typescript/src/exercicio-4.ts
@@ -22,9 +22,15 @@ const pokemon3: pokemon = {
   healthPoints: 35
 }
 
-// a) 
+// a) Instalado globalmente com npm i -g typescript e executando o comando: tsc ./nome-do-arquivo.ts
+// Instalado localmente com npm i typescript -D e executando o comando: npx tsc ./nome-do-arquivo.ts
 
-// b) Não
+// b)  Instalado globalmente com npm i -g typescript e executando o comando: tsc ./src/exercicio4.ts
+// Instalado localmente com npm i typescript -D e executando o comando: npx tsc ./src/exercicio4.ts
 
-// c) Não sei
+// c) Sim, configurando o tsconfig para olhar a pasta de entrada rootDir e a pasta de saída outDir
 
+// Instalado globalmente com npm i -g typescript e executando o comando: tsc
+// Instalado localmente com npm i typescript -D e executando o comando: npx tsc
+
+// d) O outDir, rootDir e es6. Existem diversas configs comentadas, cada uma com uma funcionalidade.
